bffd5adb70ada232d8c54bac603043f0d92f6b40
veronikaria <89734639+veronikaria@users.noreply.github.com>
2022-08-06 23:44:42 +0300

    Included holidays in trigger

--- CreateTriggerVacation.sql
+++ CreateTriggerVacation.sql
@@ -6,20 +6,20 @@ AS
 BEGIN
 DECLARE @id INT
 DECLARE @M MONEY;
-DECLARE @C INT = 10;
 DECLARE vacation_Cursor CURSOR FAST_FORWARD FOR 
-	SELECT SUM(Salary), i.VacationId
-	FROM Delivery d
-	JOIN Vacation v on v.EmployeeId=d.EmployeeId 
+	SELECT SUM(wh.SalaryWithoutTax), i.VacationId
+	FROM WorkingHours wh
+	JOIN Vacation v on v.EmployeeId=wh.EmployeeId 
 	JOIN inserted i on i.VacationId=v.VacationId
-	WHERE d.Date >= dateadd(month, datediff(month, 0, getdate()) - 12, 0)
+	WHERE wh.WorkingDate >= dateadd(month, datediff(month, 0, getdate()) - 12, 0)
 	GROUP BY i.VacationId
+
  OPEN vacation_Cursor 
  FETCH NEXT FROM vacation_Cursor into @M, @id
  WHILE @@FETCH_STATUS = 0 
  BEGIN 
 	UPDATE Vacation
-	SET Payment = @M/(365-@C)* (DATEDIFF(day, StartDate, EndDate))
+	SET Payment = @M/(365- (SELECT COUNT(*) FROM Holiday WHERE YEAR(StartDate)=YEAR(Date)))* (DATEDIFF(day, StartDate, EndDate))
 	WHERE VacationId=@id
  FETCH NEXT FROM vacation_Cursor into @M, @id
  END
