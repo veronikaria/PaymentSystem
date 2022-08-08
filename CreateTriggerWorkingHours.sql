8bab4a1b8b5f2f07cb8b35516dcc4bf6de576f44
veronikaria <89734639+veronikaria@users.noreply.github.com>
2022-08-08 21:06:37 +0300

    Updated trigger WorkingHoursInsert
    The trigger for calculating the salary, taking into account the requirements that after the dismissal of the employee, the salary is not charged to him, and considering the requirements that the basic salary cannot be less than the minimum

--- CreateTriggerWorkingHours.sql
+++ CreateTriggerWorkingHours.sql
@@ -4,23 +4,36 @@ ON WorkingHours
 AFTER INSERT
 AS
 BEGIN
-DECLARE @summ MONEY
 DECLARE @id INT
-DECLARE my_Cursor CURSOR FAST_FORWARD FOR 
-SELECT p.TariffMain*wh.HoursMain+p.TariffAdditional*wh.HoursAdditional+p.TariffHoliday*wh.HoursHoliday, i.WorkingHoursId
-	FROM Pay p
-	JOIN Employee e ON e.PostId=p.PostId
-	JOIN WorkingHours wh ON wh.EmployeeId=e.EmployeeId
-	JOIN inserted i ON i.WorkingHoursId=wh.WorkingHoursId
- OPEN my_Cursor 
- FETCH NEXT FROM my_Cursor into @summ, @id
+DECLARE @iswork BIT
+DECLARE @summ MONEY
+DECLARE working_Cursor CURSOR FAST_FORWARD FOR 
+SELECT p.TariffMain*w.HoursMain+p.TariffAdditional*w.HoursAdditional+p.TariffHoliday*w.HoursHoliday, 
+	i.WorkingHoursId, e.IsWorks
+	FROM Employee e
+	JOIN Pay p ON p.PostId=e.PostId
+	JOIN WorkingHours w ON w.EmployeeId=e.EmployeeId
+	JOIN inserted i ON i.WorkingHoursId=w.WorkingHoursId
+ OPEN working_Cursor 
+ FETCH NEXT FROM working_Cursor into @summ, @id, @iswork
  WHILE @@FETCH_STATUS = 0 
  BEGIN 
  UPDATE WorkingHours
-SET SalaryWithTax = @summ
-WHERE WorkingHours.WorkingHoursId=@id
- FETCH NEXT FROM my_Cursor into @summ, @id  
+SET SalaryWithTax = CASE
+	WHEN @iswork=0
+		THEN NULL
+	WHEN @summ < ( 
+		SELECT MinSalary FROM MinimumSalary
+		WHERE WorkingDate BETWEEN DateStart AND DateEnd
+	) THEN ( 
+		SELECT MinSalary FROM MinimumSalary
+		WHERE WorkingDate BETWEEN DateStart AND DateEnd
+	)
+	ELSE @summ
+	END
+WHERE WorkingHoursId = @id
+ FETCH NEXT FROM working_Cursor into @summ, @id, @iswork
  END
-CLOSE my_Cursor
-DEALLOCATE my_Cursor
+CLOSE working_Cursor
+DEALLOCATE working_Cursor
 END
