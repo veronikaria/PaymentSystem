4dbb7bc09fdb88b7453e807a845b68bb0fe1764e
veronikaria <89734639+veronikaria@users.noreply.github.com>
2022-08-08 19:48:56 +0300

    Created new trigger PostCreateUserAndDate

---
+++ RecordsManagement/CreateTriggerPostCreateUserAndDate.sql
@@ -0,0 +1,21 @@
+
+CREATE TRIGGER PostCreateUserAndDate
+ON Post
+AFTER INSERT
+AS
+BEGIN
+	DECLARE @id INT
+	DECLARE mycursor CURSOR FAST_FORWARD FOR 
+	SELECT PostId FROM inserted
+	OPEN mycursor
+	FETCH NEXT FROM mycursor into @id
+	WHILE @@FETCH_STATUS=0
+	BEGIN
+		UPDATE Post SET UCR = CURRENT_USER,
+		DCR=GETDATE() 
+		WHERE PostId = @id
+	FETCH NEXT FROM mycursor into @id
+	END
+CLOSE mycursor
+DEALLOCATE mycursor
+END
