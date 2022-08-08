7ecb2d31762e8c782d4e716fd24692a99dc6ac41
veronikaria <89734639+veronikaria@users.noreply.github.com>
2022-08-08 21:37:54 +0300

    Updated trigger VacationInsert
    Trigger for vacation management. When adding a record to the Vacation table, the Payment field should be calculated automatically. The Payment field must be calculated from the formula S=M/(365-C)*N described in the readme file. To perform this accounting, the VacationInsert trigger was created. With the help of the vacation_Cursor fast forward cursor, you can go through each row of the inserted table and add a value to the Payment field. If null is returned from the EmployeeVacationPerc function, then an error is returned

--- CreateTriggerVacation.sql
+++ CreateTriggerVacation.sql
@@ -5,23 +5,29 @@ AFTER INSERT
 AS
 BEGIN
 DECLARE @id INT
+DECLARE @subId INT
 DECLARE @M MONEY;
+DECLARE @C INT = 10;
 DECLARE vacation_Cursor CURSOR FAST_FORWARD FOR 
-	SELECT SUM(wh.SalaryWithoutTax), i.VacationId
-	FROM WorkingHours wh
-	JOIN Vacation v on v.EmployeeId=wh.EmployeeId 
+	SELECT SUM(Salary), i.VacationId, e.SubdivisionId 
+	FROM Delivery d
+	JOIN Vacation v on v.EmployeeId=d.EmployeeId 
+	JOIN Employee e on e.EmployeeId=v.EmployeeId
 	JOIN inserted i on i.VacationId=v.VacationId
-	WHERE wh.WorkingDate >= dateadd(month, datediff(month, 0, getdate()) - 12, 0)
-	GROUP BY i.VacationId
-
+	WHERE d.Date >= dateadd(month, datediff(month, 0, getdate()) - 12, 0)
+	GROUP BY i.VacationId, e.SubdivisionId
  OPEN vacation_Cursor 
- FETCH NEXT FROM vacation_Cursor into @M, @id
+ FETCH NEXT FROM vacation_Cursor into @M, @id, @subId
  WHILE @@FETCH_STATUS = 0 
- BEGIN 
+ BEGIN
 	UPDATE Vacation
-	SET Payment = @M/(365- (SELECT COUNT(*) FROM Holiday WHERE YEAR(StartDate)=YEAR(Date)))* (DATEDIFF(day, StartDate, EndDate))
+	SET Payment =
+	CASE WHEN dbo.EmployeeVacationPerc(StartDate, EndDate, @subId) = 1 
+	THEN @M/(365-@C)* (DATEDIFF(day, StartDate, EndDate))
+	ELSE CONVERT(int, 'ERROR Працівників одночасно у відпустці не може бути більше 15%')
+	END
 	WHERE VacationId=@id
- FETCH NEXT FROM vacation_Cursor into @M, @id
+ FETCH NEXT FROM vacation_Cursor into @M, @id, @subId
  END
 CLOSE vacation_Cursor
 DEALLOCATE vacation_Cursor
